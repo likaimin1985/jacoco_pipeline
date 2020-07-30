59bb505cf377ed59330e4a9509de0415b80f0d6e
likaimin1985 <likaimin1985@163.com>
2020-07-30 02:51:54 -0400

    jenkinsfile

---
+++ vars/jenkinsTrigger.groovy
@@ -0,0 +1,13 @@
+def call(){
+
+    workspace= "${env.JENKINS_HOME}/pipeline_script_ec"
+    //ws()人为规定工作空间，用于存放pipeline源码
+    ws("$workspace"){
+        //拉取pipeline源码
+        git credentialsId: 'c5579d06-b37e-4410-948c-0588ff764679', url: 'https://github.com/likaimin1985/jacoco_pipeline.git'
+        def node_name = env.NODE_NAME
+        load "Jenkinsfile"
+
+    }
+
+}
